9bf160e89369fc33a087fd8b32e5026bb3984c7c
Chris Wade <swade781977@yahoo.com>
2020-01-23 03:24:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if line.empty?
+    puts "The line is currently empty."
+  else
+    line.index
+end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  if katz_deli[name]
