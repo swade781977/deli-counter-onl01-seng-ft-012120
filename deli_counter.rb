1a4ba0952e023f03f5b0ccd48dff11c6d302be1d
Chris Wade <swade781977@yahoo.com>
2020-01-23 03:36:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,4 +11,7 @@ def take_a_number(katz_deli, name)
 end
 def now_serving(katz_deli)
   if katz_deli.empty?
-  puts "Currently serving #{katz_deli.first}"
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}"
+end
