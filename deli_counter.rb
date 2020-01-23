70884d08b6f446a76a9bc891063638049f6076b6
Chris Wade <swade781977@yahoo.com>
2020-01-23 03:34:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,4 +10,5 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli[index] +1} in line."
 end
 def now_serving(katz_deli)
+  if katz_deli.empty?
   puts "Currently serving #{katz_deli.first}"
